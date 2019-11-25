c875cbcdb4c7b6544ae3e754ffb2c7de2dacff72
Touseef Tariq <touseef.tariq@naxxa.io>
2019-11-25 18:44:16 +0500

    App completed

--- src/components/App.js
+++ src/components/App.js
@@ -9,25 +9,41 @@ class App extends React.Component{
         videos: [],
         selectedVideo: null
     }
+    
+    componentDidMount(){
+        this.onTermSubmit('building');
+    }
+
     onTermSubmit =async term => {
         const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         })
-        this.setState({ videos:response.data.items }); 
+        this.setState({ 
+            videos:response.data.items,
+            selectedVideo: response.data.items[0]
+         }); 
     }
 
     onVideoSelect = (video) => {
-        console.log('from the App', video);
+        this.setState({ selectedVideo: video });
     }
 
     render(){
         return(
             <div className='ui container'>
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoDetails video={this.state.selectedVideo} />
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                <div className='ui grid'>
+                    <div className='ui row'>
+                        <div className='eleven wide column'>
+                            <VideoDetails video={this.state.selectedVideo} />
+                        </div>
+                        <div className='five wide column'>
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
